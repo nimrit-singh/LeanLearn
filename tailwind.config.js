e83e3c255f0a766d48657a1d6df218b5fdc8f145
AbhinavKRNarayan <abhinavv2005@gmail.com>
2024-11-25 21:12:36 +0530

    added styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        'primary-blue': '#00A6FF',
+        'primary-green': '#7FE84D',
+        'dark-bg': '#101113',
+        'section-bg': 'rgb(58,59,61)',
+        'border': '#3A3B3D',
+      },
       fontFamily: {
         'nunito': ['Nunito', 'sans-serif'],
       },
-      colors: {
-        primary: {
-          blue: '#00A6FF',
-        }
-      }
     },
   },
   plugins: [],
