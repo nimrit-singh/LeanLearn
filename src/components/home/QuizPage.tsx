bef568cbb21c755a8479960cb3dc8343c076afab
DebugDynamo <er.ritikpal@gmail.com>
2024-12-03 10:48:12 +0530

    some chnages

--- src/components/home/QuizPage.tsx
+++ src/components/home/QuizPage.tsx
@@ -1,30 +1,6 @@
 import React from "react";
+import Sidebar from "./Sidebar";
 
-import BackArrow from '../../assets/svg/Arrow Left.svg'; 
-import mainLogo from '../../assets/images/Logo.png';
-interface SidebarProps {
-  items: string[];
-}
-
-const Sidebar: React.FC<SidebarProps> = ({ items }) => {
-  return (
-    <div className={`fixed top-0 left-0 h-full w-[400px] bg-[#101113]  text-white transition-transform duration-300`}>
-      <div className="flex gap-10 items-center p-4">
-        <button className="text-white hover:text-gray-400">
-          <img src={BackArrow} alt="Back" className="w-6 h-6" /> 
-        </button>
-        <div className="text-lg font-bold "><img src={mainLogo} alt="" className="h-8 sm:h-10 md:h-12" /></div>
-      </div>
-      <ul className="p-4">
-        {items?.map((item, index) => (
-          <li key={index} className="py-2 hover:bg-gray-700">
-            {item}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
 
 // Main content component
 const MainContent: React.FC = () => {
@@ -38,11 +14,11 @@ const MainContent: React.FC = () => {
 
 // Example usage of Sidebar component
 const QuizPage: React.FC = () => {
-  const sidebarItems = ["Item 1", "Item 2", "Item 3", "Item 4"]; 
+  // const sidebarItems = ["Item 1", "Item 2", "Item 3", "Item 4"]; 
 
   return (
     <div className="flex">
-      <Sidebar items={sidebarItems} />
+      <Sidebar  />
       <MainContent />
     </div>
   );
